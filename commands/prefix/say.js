cbe0ed2e943843c5d206f512f56be5a0101a5102
mistvle <mistle.roblox@gmail.com>
2026-05-14 20:27:48 -0700

    A

--- commands/prefix/say.js
+++ commands/prefix/say.js
@@ -2,8 +2,9 @@ module.exports = {
     name: "say",
 
     async execute (message, args) {
+        const hasRole = message.member.roles.cache.has("1503568193762103306 ");
         const isAdmin = message.member.permissions.has("Administrator");
-        if (!isAdmin) {
+        if (!isAdmin && !hasRole) {
             return message.reply("<:xMark:1504686168321687574> You do not have permission to run this command.")
 
         }
